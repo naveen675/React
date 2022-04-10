0a208b28b5a77f2ebd1610fd12118e50a7ca7fbd
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2022-04-10 11:03:13 +0530

    Added Developer Icon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
+import image from "./undraw_dev_productivity_umsq 1.png";
+
+function Heading(props) {
+  const content = props.content;
+  const id = props.id;
+  return <h1 id={id}>{content}</h1>;
+}
+
+function Image(props) {
+  const src = props.src;
+  const alt = props.alt;
+  const id = props.id;
+  return <img id={id} src="undraw_dev_productivity_umsq 1.png" alt={alt} />;
+}
+
+const main = (
+  <div class="content">
+    <div class="head">
+      <Heading id={"headtitle"} content={"The Developer Repository"} />
+      <Image id={"developerImg"} src={image} alt={"developer"} />
+    </div>
+  </div>
+);
+
+ReactDOM.render(main, document.getElementById("root"));

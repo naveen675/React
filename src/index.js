dca7a750de8266bc60ef5899f8696a1ccd174975
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2022-04-12 13:27:34 +0530

    Added Developer Form

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import devimage from "./Images/undraw_dev_productivity_umsq 1.png";
 import searchicon from "./Images/search-24px.svg";
+import giticon from "./Images/iconfinder_github_317712.png";
+import linkedicon from "./Images/iconfinder_2018_social_media_popular_app_logo_linkedin_3225190.png";
+import codecheficon from "./Images/codechef-1324440139527402917_32.png";
+import hackericon from "./Images/iconfinder_160_Hackerrank_logo_logos_4373234.png";
+import twitericon from "./Images/iconfinder_2018_social_media_popular_app_logo_twitter_3225183.png";
+import mediumicon from "./Images/iconfinder_Circled_Medium_svg5_5279113.png";
+
 function Heading(props) {
   const content = props.content;
   const id = props.id;
@@ -43,6 +50,45 @@ function Footer() {
   );
 }
 
+function Form() {
+  return (
+    <form className="devAddform">
+      <label>
+        <Image src={giticon} alt={"github"} id={"giticon"} />
+        Github
+      </label>
+      <Input type={"text"} id={"gitin"} />
+      <label>
+        <Image src={linkedicon} alt={"linkedin"} id={"linkedicon"} />
+        Linkedin
+      </label>
+      <Input type={"text"} id={"linkedin"} />
+      <label>
+        <Image src={codecheficon} alt={"codechef"} id={"codecheficon"} />
+        Codechef
+      </label>
+      <Input type={"text"} id={"codechefin"} />
+      <label>
+        <Image src={hackericon} alt={"hackerrank"} id={"hackericon"} />
+        HackerRank
+      </label>
+      <Input type={"text"} id={"hackerin"} />
+      <label>
+        <Image src={twitericon} alt={"twitter"} id={"twitericon"} />
+        Twitter
+      </label>
+      <Input type={"text"} id={"twiterin"} />
+      <label>
+        <Image src={mediumicon} alt={"medium"} id={"mediumicon"} />
+        Medium
+      </label>
+      <Input type={"text"} id={"mediumin"} />
+      <button id={"devformsubmit"}> Submit </button>
+      <button id={"devformcancel"}> Cancel </button>
+    </form>
+  );
+}
+
 const main = (
   <div className="content">
     <div className="head">
@@ -59,6 +105,7 @@ const main = (
       <p>Could not find what you are looking for ?</p>
       <button id="addDev">Add developer Info</button>
     </div>
+    <Form />
     <Footer />
   </div>
 );

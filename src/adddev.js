9b1805d0975c80c34bb40ceba0db16273dd2d8a2
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2022-04-23 23:39:33 +0530

    Created Add Dev Comp

---
+++ src/adddev.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function AddDev() {
+  const navigate = useNavigate();
+
+  return (
+    <React.Fragment>
+      <button id="addDev" onClick={() => navigate("/form")}>
+        Add developer Info
+      </button>
+    </React.Fragment>
+  );
+}
